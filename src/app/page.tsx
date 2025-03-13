070060f7a56dd7630bc8b08b62dac2937bd3734f
JanayAlam <janayalam820@gmail.com>
2025-03-13 13:39:51 +0600

    sc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,9 @@
-"use client";
-
-import { logoutApi } from "@/api/auth";
-import { useAuthStore } from "@/store/auth-store";
-import { handlePrivateApiError } from "@/utils/error-handlers";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
-import { CommonApiError } from "./_types/common/error";
+import CustomerNavbar from "@/components/ui/headers/customer-navbar";
 
 export default function Home() {
-  const router = useRouter();
-  const setUser = useAuthStore((state) => state.setUser);
-
-  const logout = async () => {
-    try {
-      await logoutApi();
-      setUser(null);
-      router.push("/admin/login");
-    } catch (err) {
-      const { data, error } = handlePrivateApiError(err as CommonApiError);
-      toast.error(data?.message || error || "Could not logout");
-    }
-  };
-
   return (
     <div className="flex flex-col gap-2">
-      <Link href="/admin/login">Go to login page</Link>
-      <Link href="/outlet-admin">Go to outlet admin page</Link>
-      <div>
-        <button onClick={logout}>Logout</button>
-      </div>
+      <CustomerNavbar />
     </div>
   );
 }
---
+++ src/components/ui/headers/customer-navbar/CustomerNavbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomerNavbar: React.FC = () => {
+  return <></>;
+};
+
+export default CustomerNavbar;

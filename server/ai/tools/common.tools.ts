0b10b107ba29c1b1017b45dc594ac42f02d1122e
JanayAlam <janayalam820@gmail.com>
2026-05-28 19:48:31 +0600

    sc

--- server/ai/tools/common.tools.ts
+++ server/ai/tools/common.tools.ts
@@ -4,7 +4,7 @@ import { eventTags } from "../../enums";
 
 export const getEventTagsTool = tool({
   name: "get_event_tags",
-  description: "Get all avaialbe tags for events",
+  description: "Get all available tags for events",
   parameters: z.object({}),
   async execute() {
     return {
